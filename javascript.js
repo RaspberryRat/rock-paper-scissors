8c41eae985342428fb0be8338eafcb8909fe8351
Graham <git@grahamirvine.com>
2022-03-10 11:37:36 -0700

    Created index.html and javascript.js and created initial function

---
+++ javascript.js
@@ -0,0 +1,8 @@
+let computerChoice;
+let playerChoice;
+
+let weapon = ['rock', 'paper', 'scissors'];
+
+function computerPlay() {
+  let randomWeapon = Math.floor(Math.random() * weapon.length);
+}
