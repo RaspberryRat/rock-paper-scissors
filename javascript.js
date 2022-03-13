40523790342d198070dc77691d3fa3ccbee5cb99
Graham <git@grahamirvine.com>
2022-03-13 14:59:55 -0600

    Fixed playGame and game() function. Appears to be randomly choosing weapon and looping properly. Also, refactoring playGame() function to be shorter and less complicated. Added or statements.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 let computerChoice = computerPlay();
-let playerChoice = 'rock';
+let playerChoice = 'Rock';
 
 
 function computerPlay() {
@@ -13,44 +13,41 @@ let computerWins = 0;
 
 function playGame(playerChoice, computerChoice) {
   computerChoice = computerPlay();
-  playerChoiceFixed = playerChoice.toLowerCase();
-  let choices = `You chose ${playerChoiceFixed} and the computer chose ${computerChoice}.`;
   //winning scenarios
-  if (playerChoiceFixed == 'rock' && computerChoice == 'scissors') {
-      return playerWins += 1;
-  } else if (playerChoiceFixed == 'scissors' && computerChoice == 'paper') {
-      return playerWins += 1;
-  } else if (playerChoiceFixed == 'paper' && computerChoice == 'rock') {
-      return playerWins += 1;
+  playerChoiceFixed = playerChoice.toLowerCase();
+  //let choices = `You chose ${playerChoiceFixed} and the computer chose ${computerChoice}.`;
+  if ((playerChoiceFixed == 'rock' && computerChoice == 'scissors') ||
+      (playerChoiceFixed == 'scissors' && computerChoice == 'paper') ||
+      (playerChoiceFixed == 'paper' && computerChoice == 'rock')) {
+      playerWins += 1;
+      console.log(`${playerChoice} beats ${computerChoice}! You win!`);
   //losing scenarios
-  } else if (playerChoiceFixed == 'rock' && computerChoice == 'paper') {
-    return computerWins += 1;
-  } else if (playerChoiceFixed == 'paper' && computerChoice == 'scissors') {
-    return computerWins += 1;
-  } else if (playerChoiceFixed == 'scissors' && computerChoice == 'rock') {
-    return computerWins += 1;
+  } else if ((playerChoiceFixed == 'rock' && computerChoice == 'paper') ||
+  (playerChoiceFixed == 'paper' && computerChoice == 'scissors') ||
+  (playerChoiceFixed == 'scissors' && computerChoice == 'rock')) {
+    computerWins =+ 1;
+    console.log(`${computerChoice} beats ${playerChoiceFixed}. You lose this round.`);
   } else if (playerChoiceFixed == computerChoice) {
-    return (`${choices} You both chose ${playerChoiceFixed}, and tied! Try again?`);
+    console.log(`You both chose ${playerChoiceFixed}, and tied! Try again?`);
   } else {
-    return ("You must enter 'rock', 'paper', or 'scissors'.");
+    console.log("You must enter 'rock', 'paper', or 'scissors'.");
   }
 }
-
 
 function game() {
   for (let i = 1; i <= 5; i++) {
     let round = i;
     console.log(`Round # ${round}.`);
-    console.log(playGame(playerChoice, computerChoice));
-    console.log(`You chose ${playerChoice} and your opponent chose ${computerChoice}`);
-    console.log(`The winner of round # ${round} is `);
+    playGame(playerChoice, computerChoice);
+    console.log(`The score is ${playerWins} for you and ${computerWins} for your opponent.`);
   }
   if (playerWins > computerWins) {
     console.log(`You won ${playerWins} rounds. Your opponent won ${computerWins} rounds. You won the game! Great job!`);
   } else if (playerWins < computerWins) {
     console.log(`You won ${playerWins} rounds. Your opponent won ${computerWins} rounds. You lost. So sad.`);
   } else {
-    console.log(`You won ${playerWins} rounds. Your opponent won ${computerWins} rounds. Whew a tie game! Try again?`)}
+    console.log(`You won ${playerWins} rounds. Your opponent won ${computerWins} rounds. Whew a tie game! Try again?`);
+  }
 }
 
-console.log(game());
+game();
